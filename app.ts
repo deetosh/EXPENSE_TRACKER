7977dd8e8dc9a1a4eb5d7a0cee35672bb2ae2b5f
deetosh <deetoshkuila18@gmail.com>
2025-02-02 02:17:08 +0530

    added cors options

--- app.ts
+++ app.ts
@@ -21,7 +21,10 @@ app.use(session({
   cookie: { secure: false }    // Set to true if using HTTPS
 }));
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookie());

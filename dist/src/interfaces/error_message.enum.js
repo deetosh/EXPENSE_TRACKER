11b4bf71357a5c35c665ddb1227876879ab3bb3b
deetosh <deetoshkuila18@gmail.com>
2025-02-08 14:13:42 +0530

    removed the dist folder

--- dist/src/interfaces/error_message.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.eErrorMessage = void 0;
-var eErrorMessage;
-(function (eErrorMessage) {
-    eErrorMessage["ServerError"] = "Server error occured";
-    eErrorMessage["Unauthorised"] = "Unauthorised access";
-})(eErrorMessage || (exports.eErrorMessage = eErrorMessage = {}));
